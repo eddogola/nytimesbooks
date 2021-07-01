ae5f7f72f8234ff37d0df639f52503d6495d92e6
Ogola Eddy <eddogola01@gmail.com>
2021-07-01 04:25:24 +0300

    Client struct and constructor

---
+++ client.go
@@ -0,0 +1,31 @@
+package books
+
+import "net/http"
+
+type Client struct {
+	base       string
+	apiKey     string
+	HTTPClient *http.Client
+}
+
+type OptionFunc func(*Client)
+
+func NewClient(apiKey string, options ...OptionFunc) *Client {
+	c := &Client{
+		base:       "https://api.nytimes.com/svc/books/v3",
+		apiKey:     apiKey,
+		HTTPClient: http.DefaultClient,
+	}
+
+	for _, option := range options {
+		option(c)
+	}
+
+	return c
+}
+
+func withHTTPClient(httpc *http.Client) OptionFunc {
+	return func(c *Client) {
+		c.HTTPClient = httpc
+	}
+}
