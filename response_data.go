26bace65202f2d63b0515d7df021fa901794a96f
Ogola Eddy <eddogola01@gmail.com>
2021-07-04 13:41:43 +0300

    Add list history structure

--- response_data.go
+++ response_data.go
@@ -1,4 +1,4 @@
-type Lists struct {
+type List struct {
 	Status       string `json:"status"`
 	Copyright    string `json:"copyright"`
 	NumResults   int    `json:"num_results"`
@@ -39,7 +39,7 @@ type Lists struct {
 	} `json:"results"`
 }
 
-type ListsByDate struct {
+type ListByDate struct {
 	Status       string `json:"status"`
 	Copyright    string `json:"copyright"`
 	NumResults   int    `json:"num_results"`
@@ -81,3 +81,43 @@ type ListsByDate struct {
 		Corrections []struct{} `json:"corrections"`
 	} `json:"results"`
 }
+
+type ListHistory struct {
+	Status     string `json:"status"`
+	Copyright  string `json:"copyright"`
+	NumResults int    `json:"num_results"`
+	Results    []struct {
+		Title           string `json:"title"`
+		Description     string `json:"description"`
+		Contributor     string `json:"contributor"`
+		Author          string `json:"author"`
+		ContributorNote string `json:"contributor_note"`
+		Price           int    "price"
+		AgeGroup        string `json:"age_group"`
+		Publisher       string `json:"publisher"`
+		ISBNs           []struct {
+			ISBN10 string `json:"isbn10"`
+			ISBN13 string `json:"isbn13"`
+		} `json:"isbns"`
+		RanksHistory []struct {
+			PrimaryISBN10   string `json:"primary_isbn10"`
+			PrimaryISBN13   string `json:"primary_isbn13"`
+			Rank            int    `json:"rank"`
+			ListName        string `json:"list_name"`
+			DisplayName     string `json:"display_name"`
+			PublishedDate   string `json:"published_date"`
+			BestsellersDate string `json:"bestsellers_date"`
+			WeeksOnList     int    `json:"weeks_on_list"`
+			RanksLastWeek   int    `json:"ranks_last_week"`
+			Asterisk        int    `json:"asterisk"`
+			Dagger          int    `json:"dagger"`
+		} `json:"rank_history"`
+		Reviews []struct {
+			BookReviewLink     string `json:"book_review_link"`
+			FirstChapterLink   string `json:"first_chapter_link"`
+			SundayReviewLink   string `json:"sunday_review_link"`
+			ArticleChapterLink string `json:"article_chapter_link"`
+		} `json:"reviews"`
+	} `json:"results"`
+}
+
