1c9defe6aaa192fccc7a3175c0cb5034d8a881b5
Ogola Eddy <eddogola01@gmail.com>
2021-07-04 19:48:48 +0300

    Set api endpoints

---
+++ endpoints.go
@@ -0,0 +1,21 @@
+package books
+
+const (
+	// ListsEndpoint is the endpoint used to Get Best Sellers list. If no date is provided returns the latest list.
+	ListsEndpoint = "/lists.json"
+
+	// ListsByDateEndpoint is the endpoint to Get Best Sellers list by date.
+	ListsByDateEndpoint = "/lists/%v/%v.json"
+
+	// HistoryEndpoint is the endpoint used to Get Best Sellers list history.
+	HistoryEndpoint = "/lists/best-sellers/history.json"
+
+	// NamesEndpoint is the endpoint used to Get Best Sellers list names.
+	NamesEndpoint = "/lists/names.json"
+
+	// OverviewEndpoint is the endpoint used to Get top 5 books for all the Best Sellers lists for specified date.
+	OverviewEndpoint = "/lists/overview.json"
+
+	// ReviewsEndpoint is the endpoint for Getting book reviews.
+	ReviewsEndpoint = "/reviews.json"
+)
