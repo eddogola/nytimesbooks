5e61cc93c530870985bee7daba2590cc77f52671
Ogola Eddy <eddogola01@gmail.com>
2021-07-04 12:42:25 +0300

    Mock http client to test Get

--- client_test.go
+++ client_test.go
@@ -1,6 +1,9 @@
 package books
 
 import (
+	"bytes"
+	"context"
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -21,4 +24,41 @@ func TestNewClient(t *testing.T) {
 			t.Errorf("NewClient(\"apikey\").HTTPClient == %v, want %v", c.HTTPClient, httpc)
 		}
 	})
+}
+
+// mock http client
+type MockClient struct {
+	MockDo func(*http.Request) (*http.Response, error)
+}
+
+func (mc *MockClient) Do(req *http.Request) (*http.Response, error) {
+	return mc.MockDo(req)
+}
+
+func TestGet(t *testing.T) {
+	// setup mock client
+	mc := &MockClient{
+		MockDo: func(r *http.Request) (*http.Response, error) {
+			content := "jetfuel"
+			respBody := ioutil.NopCloser(bytes.NewReader([]byte(content)))
+			return &http.Response{
+				Body: respBody,
+			}, nil
+		},
+	}
+	c := NewClient("apikey", WithHTTPClient(mc))
+	resp, err := c.Get(context.Background(), "someplace.com")
+	if err != nil {
+		t.Errorf("Got unexpected error %v", err)
+	}
+	defer resp.Body.Close()
+
+	want := "jetfuel"
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	got := buf.String()
+
+	if want != got {
+		t.Errorf("got %v expected %v", got, want)
+	}
 }
