3345b206a5591ee45fbc1a32052f47edfa1c0159
Ogola Eddy <eddogola01@gmail.com>
2021-07-06 17:31:41 +0300

    Test getbestsellersbydate func

--- client_test.go
+++ client_test.go
@@ -134,7 +134,7 @@ func TestGetBestSellersList(t *testing.T) {
 	mc := &MockClient{
 		func(r *http.Request) (*http.Response, error) {
 			body := ioutil.NopCloser(bytes.NewReader([]byte(jsonData)))
-			
+
 			return &http.Response{Body: body}, nil
 		},
 	}
@@ -150,6 +150,34 @@ func TestGetBestSellersList(t *testing.T) {
 		t.Errorf("unexpected error: %v", err)
 	}
 
+	if !reflect.DeepEqual(got, &want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestGetBestSellersListByDate(t *testing.T) {
+	jsonData := `{  "status": "OK",  "copyright": "Copyright (c) 2019 The New York Times Company.  All Rights Reserved.",  "num_results": 15,  "last_modified": "2015-12-25T13:05:20-05:00",  "results": {    "list_name": "Trade Fiction Paperback",    "bestsellers_date": "2015-12-19",    "published_date": "2016-01-03",    "display_name": "Paperback Trade Fiction",    "normal_list_ends_at": 10,    "updated": "WEEKLY",    "books": [      {        "rank": 1,        "rank_last_week": 0,        "weeks_on_list": 60,        "asterisk": 0,        "dagger": 0,        "primary_isbn10": "0553418025",        "primary_isbn13": "9780553418026",        "publisher": "Broadway",        "description": "Separated from his crew, an astronaut embarks on a quest to stay alive on Mars. The basis of the movie.",        "price": 0,        "title": "THE MARTIAN",        "author": "Andy Weir",        "contributor": "by Andy Weir",        "contributor_note": "",        "book_image": "http://du.ec2.nytimes.com.s3.amazonaws.com/prd/books/9780804139038.jpg",        "amazon_product_url": "http://www.amazon.com/The-Martian-Novel-Andy-Weir-ebook/dp/B00EMXBDMA?tag=thenewyorktim-20",        "age_group": "",        "book_review_link": "",        "first_chapter_link": "",        "sunday_review_link": "",        "article_chapter_link": "",        "isbns": [          {            "isbn10": "0804139024",            "isbn13": "9780804139021"          }        ]      }    ],    "corrections": []  }}`
+
+	// setup mock http client
+	mc := &MockClient{
+		func(r *http.Request) (*http.Response, error) {
+			body := ioutil.NopCloser(bytes.NewReader([]byte(jsonData)))
+
+			return &http.Response{Body: body}, nil
+		},
+	}
+
+	c := NewClient("apikey", WithHTTPClient(mc))
+	got, err := c.GetBestSellersListByDate("2020-07-06", "hardcover-fiction", nil)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	var want ListByDate
+	err = json.Unmarshal([]byte(jsonData), &want)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	fmt.Print(got.Results.Books[0].AmazonProductURL)
 	if !reflect.DeepEqual(got, &want) {
 		t.Errorf("got %v want %v", got, want)
 	}
